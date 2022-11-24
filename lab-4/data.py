561d280d80292823ba5912f1dfc9c3a6b552e0cf
Dmytro <vdude112358@gmail.com>
2022-11-24 23:21:54 +0200

    Add data

---
+++ lab-4/data.py
@@ -0,0 +1,37 @@
+candidates = [
+    {
+        "_id": 1,
+        "name": "First Candidate"
+    },
+    {
+        "_id": 2,
+        "name": "Second Candidate"
+    }
+]
+
+voters = [
+    {
+        "_id": 43212,
+        "full_name": "Natasha Williams",
+        "age": 25,
+        "n": 4
+    },
+    {
+        "_id": 12091,
+        "full_name": "Jake Alexander",
+        "age": 19,
+        "n": 3
+    },
+    {
+        "_id": 13291,
+        "full_name": "Truman Powell",
+        "age": 37,
+        "n": 2
+    },
+    {
+        "_id": 91125,
+        "full_name": "Huxley Foster",
+        "age": 29,
+        "n": 1
+    },
+]
