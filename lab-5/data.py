5ce5582bc41ed2f5c9d0eec6e3a66337a0b4f3d3
Dmytro <vdude112358@gmail.com>
2022-12-01 18:53:24 +0200

    L5: Add data file

---
+++ lab-5/data.py
@@ -0,0 +1,52 @@
+candidates = [
+    "First Candidate",
+    "Second Candidate"
+]
+
+voters = [
+    {
+        "_id": 43212,
+        "full_name": "Natasha Williams",
+        "age": 25
+    },
+    {
+        "_id": 73645,
+        "full_name": "Valente Edwards",
+        "age": 34
+    },
+    {
+        "_id": 92756,
+        "full_name": "Pamela Lopez",  # too young
+        "age": 17
+    },
+    {
+        "_id": 12091,
+        "full_name": "Jake Alexander",
+        "age": 19
+    },
+    {
+        "_id": 43325,
+        "full_name": "Vince Watson",  # double
+        "age": 22
+    },
+    {
+        "_id": 43325,
+        "full_name": "Vince Watson",  # double
+        "age": 22
+    },
+    {
+        "_id": 13291,
+        "full_name": "Truman Powell",
+        "age": 37
+    },
+    {
+        "_id": 61325,
+        "full_name": "Felicity Thompson",
+        "age": 35
+    },
+    {
+        "_id": 91125,
+        "full_name": "Huxley Foster",
+        "age": 29
+    },
+]
