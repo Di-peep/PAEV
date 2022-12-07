649ea53e3a53a96a4d6a818130851b037eecccde
Dmytro <vdude112358@gmail.com>
2022-12-07 15:08:57 +0200

    L6: Add bulletin

---
+++ lab-6/bulletin.py
@@ -0,0 +1,5 @@
+class Bulletin:
+    def __init__(self, choice, first_bit_gen_value, voter_id):
+        self.choice = choice
+        self.voter_id = voter_id
+        self.first_bit_gen_value = first_bit_gen_value
